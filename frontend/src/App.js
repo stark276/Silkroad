b9a165729ead522046f1e5bc9f4a3a6e35bbbbf5
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-04-10 20:14:07 -0700

    rating and router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,26 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen'
+
 
 
 
 function App() {
   return (
-    <div >
+    <Router >
       <Header />
-      <main>
-        <Container className='py-3'>
-       < HomeScreen />
+      <main className='py-3'>
+        <Container >
+       <Route path='/' component={HomeScreen} exact/>
+        <Route path='/product/:id' component={ProductScreen}/>
 
         </Container>
         
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
