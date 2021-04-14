e8ecab52d1ea1fbea8755124294608cc91ae2547
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-04-13 18:13:52 -0700

    orduct screen

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -17,7 +17,7 @@ const products = [
         'name': 'Plov',
         'image': '/images/plov.jpg',
         'description':
-        'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+        'Steemed Ingredients: rice, carrot, onion, cumin, salt, black pepper, garlic, lamb (beef optional)',
         'brand': 'Apple',
         'category': 'Electronics',
         'price': 599.99,
@@ -38,44 +38,6 @@ const products = [
         'rating': 3,
         'numReviews': 12,
     },
-    {
-        '_id': '4',
-        'name': 'Sony Playstation 4 Pro White Version',
-        'image': '/images/playstation.jpg',
-        'description':
-        'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
-        'brand': 'Sony',
-        'category': 'Electronics',
-        'price': 399.99,
-        'countInStock': 11,
-        'rating': 5,
-        'numReviews': 12,
-    },
-    {
-        '_id': '5',
-        'name': 'Logitech G-Series Gaming Mouse',
-        'image': '/images/mouse.jpg',
-        'description':
-        'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
-        'brand': 'Logitech',
-        'category': 'Electronics',
-        'price': 49.99,
-        'countInStock': 7,
-        'rating': 3.5,
-        'numReviews': 10,
-    },
-    {
-        '_id': '6',
-        'name': 'Amazon Echo Dot 3rd Generation',
-        'image': '/images/alexa.jpg',
-        'description':
-        'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
-        'brand': 'Amazon',
-        'category': 'Electronics',
-        'price': 29.99,
-        'countInStock': 0,
-        'rating': 4,
-        'numReviews': 12,
-    },
+    
 ]
  export default products
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,53 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card, Form } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-function ProductScreen() {
+function ProductScreen({match}) {
+  const product = products.find((p)=> p._id == match.params.id)
   return (
     <div>
-      Product
+      <Link to="/" className="btn btn-dark my-3">Go Back</Link>
+      <Row>
+        <Col md={6}>
+        <Image src={product.image} alt={product.name} fluid/>
+        </Col>
+        <Col md={3}>
+            <ListGroup varian="flush">
+                <ListGroup.Item>
+                    <h3>{product.name}</h3>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`} color={"#f8e825"}/>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    Price: {product.price}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    Description: {product.description}
+                </ListGroup.Item>
+
+            </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                  Price:
+                  </Col>
+                  <Col>
+                  <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
     </div>
   )
 }
