72435b241ecd42d0fed7e368e4aa799358ac3bb0
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-04-18 21:24:18 -0700

    redux home screen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,7 +4,7 @@ PRODUCT_LIST_REQUEST,
 PRODUCT_LIST_SUCCESS ,
 PRODUCT_LIST_FAIL  } from '../constans/productConsants'
 
-const listProduct = () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
   try{
     dispatch({type : PRODUCT_LIST_REQUEST})
 
