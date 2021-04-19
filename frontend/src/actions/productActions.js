300001c5ae0bd456aad60a8fb12841bf33d1603a
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-04-19 03:24:46 -0700

    product details reducer and action added

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,8 +1,12 @@
 import axios from 'axios'
 import { 
-PRODUCT_LIST_REQUEST,
-PRODUCT_LIST_SUCCESS ,
-PRODUCT_LIST_FAIL  } from '../constans/productConsants'
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS ,
+  PRODUCT_LIST_FAIL,
+
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS ,
+  PRODUCT_DETAILS_FAIL   } from '../constans/productConsants'
 
 export const listProducts = () => async (dispatch) => {
   try{
@@ -19,6 +23,29 @@ export const listProducts = () => async (dispatch) => {
     dispatch({
 
       type:PRODUCT_LIST_FAIL,
+      payload:error.response && error.response.data.messaget
+      ? error.response.data.message
+      : error.message,
+    })
+
+  }
+}
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try{
+    dispatch({type : PRODUCT_DETAILS_REQUEST})
+
+    const {data} = await axios.get(`/api/products/${id}`)
+
+    dispatch({
+      type:PRODUCT_DETAILS_SUCCESS,
+      payload: data
+      })
+
+  }catch(error){
+    dispatch({
+
+      type:PRODUCT_DETAILS_FAIL,
       payload:error.response && error.response.data.message
       ? error.response.data.message
       : error.message,
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,25 +1,32 @@
 import React, {useState, useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import  axios  from 'axios'
+import { listProductDetails } from '../actions/productActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 function ProductScreen({match}) {
-  const [product, setProduct] = useState([])
+  const dispatch = useDispatch()
+  const productDetails = useSelector(state => state.productDetails)
+  const {loading, error, product} = productDetails
 
   useEffect(() =>{
-    async function fetchProduct(){
-        const {data} = await axios.get(`/api/products/${match.params.id}`)
-        setProduct(data)
-    }
-    fetchProduct()
+    dispatch(listProductDetails(match.params.id))
+
   }, [])
 
 
   return (
     <div>
       <Link to="/" className="btn btn-dark my-3">Go Back</Link>
-      <Row>
+      {loading ? 
+        <Loader/>
+        : error
+        ? <Message variant = 'danger'>{error}</Message>
+        :(
+          <Row>
         <Col md={6}>
         <Image src={product.image} alt={product.name} fluid/>
         </Col>
@@ -71,6 +78,10 @@ function ProductScreen({match}) {
           </Card>
         </Col>
       </Row>
+
+        )
+      }
+      
     </div>
   )
 }
