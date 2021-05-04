cb725411200973c2282480b1248801e15a9e7954
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-05-04 08:36:37 -0700

    users api updated

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView
-)
+
 
 urlpatterns = [
-  path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
   path('', views.getRoutes, name='routes'),
+
+  path('users/profile/', views.getUserProfile, name='user-profile'),
+  path('users/', views.getUsers, name='users'),
+
   path('products/', views.getProducts, name='products'),
   path('products/<str:pk>', views.getProduct, name='product'),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,16 +1,51 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from django.http import JsonResponse
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import Product
 from .products import products
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+  def validate(self, attrs):
+
+    data = super().validate(attrs)
+    
+    serializer = UserSerializerWithToken(self.user).data
+
+    for k, v in serializer.items():
+      data[k] = v
+
+    return data 
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 # Create your views here.
 @api_view(['GET'])
 def getRoutes(request):
   return Response('Hello')
+
+#User
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+  user = request.user
+  serializer = UserSerializer(user, many=False)
+  return Response(serializer.data)
+
+#GETTING USERS
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+
+def getUsers(request):
+  users = User.objects.all()
+  serializer = UserSerializer(users, many=True)
+  return Response(serializer.data)
 
 #PRODUCTS
 @api_view(['GET'])
